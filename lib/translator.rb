07d94eb544e123fcd5e03369ccfb89c899791ab2
Tyree Williams <tyreewilliams515@gmail.com>
2020-01-03 11:36:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,22 @@
 # require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(file)
+  direct_file = YAML.load_file(file)
+  result = {}
+  result[:get_emoticon] = {}
+  result[:get_meaning] = {}
+  direct_file.each { |m, (eng, jap)|
+    direct_file[m][jap].each { |j|
+    
+    }
+    # if !result[:get_meaning].has_key?(m)
+    #   result[:get_meaning] = [jap]
+    # if !result[:get_meaning].include?(jap)
+    # end
+  }
+  return result
 end
 
 def get_japanese_emoticon
