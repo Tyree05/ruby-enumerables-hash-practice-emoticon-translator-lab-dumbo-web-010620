d439732b97693aa3604e36714e08d552ea2a7014
Tyree Williams <tyreewilliams515@gmail.com>
2020-01-03 12:02:02 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,10 +7,10 @@ def load_library(file)
   result = {}
   result[:get_emoticon] = {}
   result[:get_meaning] = {}
-  direct_file.each { |m, (eng, jap)|
+  direct_file.map { |m, (eng, jap)|
     # binding.pry
     # if !result[:get_meaning].has_key?(m)
-      result[:get_meaning] = [jap, jap]
+      result[:get_meaning] = [jap]
     # if !result[:get_meaning].include?(jap)
     # end
   }
