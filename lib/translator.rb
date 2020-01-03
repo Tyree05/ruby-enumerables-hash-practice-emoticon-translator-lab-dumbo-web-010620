ec0108fd184a219437938f67e2d0896a6bf2b9de
Tyree Williams <tyreewilliams515@gmail.com>
2020-01-03 13:02:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,12 +29,12 @@ end
 def get_japanese_emoticon(file, emoji)
    jap_directory = load_library(file)
    jap_emo = "Sorry, that emoticon was not found"
-   jap_directory[:get_emoticon].each { |(e,j)|
+  #  jap_directory[:get_emoticon].each { |(e,j)|
      if jap_directory[:get_emoticon].has_key?(emoji)
        jap_emo = jap_directory[:get_emoticon][emoji]
      end
     #  binding.pry
-   }
+  #  }
  return jap_emo
 end
 
